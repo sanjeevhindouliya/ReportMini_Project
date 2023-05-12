c41a90518af3c85ed908c22409e0385842a62641
sanjeev <sanjeevhindouliya@gmail.com>
2023-05-12 00:07:03 -0700

    reportMini

---
+++ src/main/java/com/sanjeev/service/ReportService.java
@@ -0,0 +1,25 @@
+package com.sanjeev.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.sanjeev.entity.CitizenPlan;
+import com.sanjeev.request.SearchRequest;
+
+public interface ReportService {
+
+	public List<String> getPlanNames();
+	
+	public List<String> getPlanStatuses();
+	
+	public List<CitizenPlan> search(SearchRequest request);
+	
+	public boolean exportExcel(HttpServletResponse response) throws Exception;
+	
+	public boolean exportPdf(HttpServletResponse response) throws Exception;
+	
+	
+	
+	
+}
